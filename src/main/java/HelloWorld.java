1eed693361098bffc465a74ccdee3d72698fb8fa
JhonatanMedina <95430245+JhonatanMedina@users.noreply.github.com>
2021-12-08 11:35:19 -0600

    Update HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -9,6 +9,6 @@ public class HelloWorld implements SparkApplication {
 
 	@Override
 	public void init() {
-		get("/hello", (req, res) -> "Hola Mundo Nuevo x4");
+		get("/hello", (req, res) -> "Hola Mundo Nuevo x5");
 	}
 }
